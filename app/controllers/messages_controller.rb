067f8dc7cfd536a31cbf84f3592f5857e66662fd
h_tsumuraya <hironori.tsumuraya@kiramex.com>
2016-03-23 00:49:54 +0000

    edit delete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,14 @@
 class MessagesController < ApplicationController
+  
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     @message = Message.new
     # すべて取得
     @messages = Message.all
   end
   
+  # 作成
   def create
     @message = Message.new(message_params)
     
@@ -19,9 +23,35 @@ class MessagesController < ApplicationController
     
   end
   
+  # 編集
+  def edit
+  end
+  
+  # 更新
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: "メッセージを編集しました"
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render "edit"
+    end
+  end
+  
+  # 削除
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: "メッセージを削除しました"
+  end
+  
   private
+  
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
   
 end
